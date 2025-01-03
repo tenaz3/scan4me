a9378fbf995ae72aaad123e241c9f7eea4982bb8
Nilson Magalhaes Junior <tenaz3.comp@gmail.com>
2025-01-03 14:06:00 -0300

    Gitaction deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://scan4me.com',
+    site: 'https://scan4me.com.br',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         shikiConfig: {
